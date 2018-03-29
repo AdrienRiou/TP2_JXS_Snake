1ca628e411d420ea4b4e33bb3997ac2ba1672365
Adrien Riou <adr.riou@gmail.com>
2018-03-29 15:50:23 +0200

    Question 3

--- Game.ts
+++ Game.ts
@@ -3,11 +3,13 @@ export class Game {
     canvasContext : CanvasRenderingContext2D;
     gridWidth : number;
     gridHeight : number;
+    initialSnake : Snake;
 
     constructor(public canvas : HTMLCanvasElement, public speed : number, public gridSize : number = 5) {
         this.gridWidth = canvas.width / gridSize;
         this.gridHeight = canvas.height / gridSize;
         this.canvasContext = canvas.getContext("2d");
+        this.initialSnake = new Snake(3);
 
         // TODO : listen to user interaction
     }
@@ -17,7 +19,7 @@ export class Game {
      */
     start() {
         // TODO : initialize game
-
+        this.draw();
         this.animate(); // Start animation
     }
 
@@ -54,4 +56,46 @@ export class Game {
         console.log("update")
     }
 
+    draw() {
+        this.initialSnake.draw(this.canvasContext);
+    }
+
+}
+
+class SnakePart {
+    x : number;
+    y : number;
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    draw(ctx) {
+        ctx.beginPath();
+        ctx.rec(this.x, this.y, 5, 5);
+        ctx.stroke();
+    }
+}
+
+class Snake {
+    tabPart;
+    size : number;
+    constructor(size) {
+        this.tabPart = new Array();
+        this.size = size;
+    }
+
+    getSnakeSize() {
+        return this.size;
+    }
+
+    addSnakePart(part) {
+        this.tabPart.push(part);
+    }
+
+    draw(ctx) {
+        this.tabPart.forEach(element => {
+            element.draw(ctx);
+        });
+    }
 }
